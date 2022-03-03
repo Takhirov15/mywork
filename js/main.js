51e1908bb291d4e7dd1f2b0743da3e55a615b3a6
Aziz <realtf15@gmail.com>
2022-03-03 20:46:01 +0500

    initial git

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Buger-menu
+
+const navToggle = document.querySelector("#navToggle");
+const navClosedIcon = document.querySelector("#navClosed");
+const navOpenIcon = document.querySelector("#navOpen");
+const navIcon = document.querySelectorAll(".navIcon");
+const nav = document.querySelector("nav");
+
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  navIcon.forEach((icon) => {
+    icon.classList.toggle("hidden");
+  });
+});
+
+
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc() {
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+});
